39aca514f50068ced49f4542cb7e1cb813a18c99
Pawelkaniak <pawkaniak@gmail.com>
2018-05-08 21:47:48 +0200

    initial commit, customize express, add index pug

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+
+app.use('/static', express.static(__dirname + '/public'))
+
+app.set('views', __dirname + '/views')
+app.set('view engine', 'pug')
+
+app.get('/', (req, res) => {
+    res.render('index');
+})
+
+app.listen(3000, () => {
+    console.log('listening on port 3000');
+})
