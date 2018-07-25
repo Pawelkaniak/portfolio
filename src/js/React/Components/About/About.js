5d56261cb97b0c0bca3ff2778dbd8722035bcfbb
Pawelkaniak <pawkaniak@gmail.com>
2018-07-25 23:28:03 +0200

    add some style and new component

--- src/js/React/Components/About/About.js
+++ src/js/React/Components/About/About.js
@@ -1,12 +1,8 @@
 import React from 'react';
 
 class About extends React.Component {
-    state = {
-        isFajno: 'ano jest'
-    }
 
     render() {
-        console.log(this.state.isFajno)
         return (
             <div>
                 <h1>About</h1>
--- src/js/React/Components/MainPage/MainPage.js
+++ src/js/React/Components/MainPage/MainPage.js
@@ -1,35 +1,37 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
 import { connect } from 'react-redux';
 import * as constants from '../../Constants'
 import { TestAction } from '../../Actions/Actions'
+import InfoBox from './InfoBox/InfoBox';
 
 const styles = require('./mainPage.scss');
 
 class MainPage extends React.Component {
     state = {
-        isTextVisible: false
+        isBoxVisible: false,
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat'
     }
-
 
     onTest() {
         this.props.testClick();
         this.setState(prevState => ({
-            isTextVisible: !prevState.isTextVisible
+            isBoxVisible: !prevState.isBoxVisible
         }))
     }
+
     render() {
         return (
             <div>
-                <h1 className={this.state.isTextVisible ? styles.btnRed : styles.btnBlack }>MainPage</h1>
+                <h1 className={this.state.isBoxVisible ? styles.btnRed : styles.btnBlack }>MainPage</h1>
                 <div>
                     <button onClick={this.onTest.bind(this)}>
                         ToggleButton
                     </button>
                     {
                         this.props.TestReducer.test ?
-                            (<div>
-                               <h1> TestButton </h1>
-                            </div>)
+                            <InfoBox
+                                text={this.state.text}
+                            />
                             :
                             null
                     }
